7f6cef26130a27987fbad0c715278812f97c73f3
tortshwar <uhhhhhhh>
2019-04-28 01:26:11 -0700

    woohooooo

---
+++ assets/js/test.js
@@ -0,0 +1,9 @@
+function dark() {
+  if (document.body.style.backgroundColor == 'rgb(255, 255, 255)') {
+      
+    document.body.style.backgroundColor = '#000';
+  }
+  else {
+    document.body.style.backgroundColor = 'rgb(255, 255, 255)';
+  }
+}
